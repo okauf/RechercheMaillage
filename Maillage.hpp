0ce4cdb21041961024d2b63a6f67051551c1655f
okauf <olivia.kaufmann@web.de>
2017-11-29 16:18:57 +0100

    main

--- Maillage.hpp
+++ Maillage.hpp
@@ -15,42 +15,36 @@ private:
     T3<double>* sommets;
     T3<int>* triangles;
 public:
-    Maillage(char* const input):sommets(LoadNodes(input)), triangles(LoadTriangles(input)){};
+	Maillage(char* const input):sommets(LoadNodes(input)), triangles(LoadTriangles(input)){};
+	
+	T3<double>* GetSommets() {return sommets;}
+	T3<int>* GetTriangles() {return triangles;}
 };
+
 T3<double>* LoadNodes(char* const input){
     
-    int numbSommets;
-    string   line;
-    
+    string line;
     
     fstream mshmaillage;
     mshmaillage.open(input);
     
-    
     //save the first line in line
     getline(mshmaillage,line);
-    
     //searching for the number
     while(line != "# Nombre de sommets"){
         getline(mshmaillage,line);
     }
-    
     getline(mshmaillage,line);
-    
     // saving the number of sommets
-    numbSommets = stoi(line);
+    int numbSommets = stoi(line);
     
-    
-    
-    
-    //generating the tableau of T3
+    //generating the tableau of R3
     T3<double> * tab = new T3<double>[numbSommets];
     
-    
-    int i = 0;
     //ignoring the next line
     getline(mshmaillage,line);
     
+	int i = 0;
     while(i < numbSommets){
         getline(mshmaillage,line);
         double a,b,c;
@@ -64,20 +58,15 @@ T3<double>* LoadNodes(char* const input){
     }
     
     mshmaillage.close();
-    
-    
     return tab;
 }
 
 T3<int>* LoadTriangles(char* const input){
-    
-    int numbTri;
+	
     string line;
-    
     
     fstream mshmaillage;
     mshmaillage.open(input);
-    
     
     //save the first line in line
     getline(mshmaillage,line);
@@ -85,17 +74,16 @@ T3<int>* LoadTriangles(char* const input){
     //searching for the number
     while(line != "# Nombre de triangle"){
         getline(mshmaillage,line);
-        
     }
     getline(mshmaillage,line);
     
     // saving the number of triangles
-    numbTri = stoi(line);
+    int numbTri = stoi(line);
     
     //generating the tableau of triangles
     T3<int>* triangles = new T3<int>[numbTri];
     
-    //ignoring the next line
+    //ignoring the nextline
     getline(mshmaillage,line);
     
     int i = 0;
@@ -107,7 +95,6 @@ T3<int>* LoadTriangles(char* const input){
         linestream >> a >>  b >> c;
         triangles[i] = T3<int>(a,b,c);
         i++;
-        
     }
     mshmaillage.close();
     
--- main.cpp
+++ main.cpp
@@ -5,7 +5,16 @@ using namespace std;
 int main(){
     
     Maillage m("maillage1.msh");
-	 T3<double> vec;
-	 T3<int> vec2(1,2,3);
+	T3<double>* Sommets = m.GetSommets();
+	T3<int>* Triangles = m.GetTriangles();
+	
+	for (int i = 0; i < 30; i++){
+		cout << Sommets[i] << endl;
+	}
+	
+	for (int i = 0; i < 30; i++){
+		cout << Triangles[i] << endl;
+	}
+	
     return 0;
 }
