7798ada0c4f75ae389a167be0c5154ce52154da4
okauf <olivia.kaufmann@web.de>
2017-11-29 21:34:26 +0100

    Lexicographical order, Adjacency, Promenade
    Lexi Order um Liste zu sortieren
    Adjacency: Ansatz mit Liste, jedes Dreieck dreimal und lexi order
    Anfang von Promenade, ToDo: Abbruchbedingungen überlegen

--- Maillage.hpp
+++ Maillage.hpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <cstdlib>
 #include <iostream>
+#include <list>
 using namespace std;
 
 class Maillage{
@@ -103,6 +104,55 @@ T3<int>* LoadTriangles(char* const input){
     mshmaillage.close();
     
     return triangles;
+}
+
+T3<int> Adjacency(const T3<int> & T, int i){
+	
+	assert(i <= 2 && i >= 0);
+	
+	// initialisation
+	list<T3<int>> l;
+	int n0, n1, n2, m01, m02, m12, M01, M02, M12;
+	for (int i = 0; i < numbTri; i++){
+		n0 = triangles[i][0];
+		n1 = triangles[i][1];
+		n2 = triangles[i][2];
+		m01 = min(n0,n1); m02 = min(n0,n2); m12 = min(n1,n2);
+		M01 = max(n0,n1); M02 = max(n0,n2); M12 = max(n1,n2);
+		l.push_front(T3<int>(m01,M01,n2));
+		l.push_front(T3<int>(m02,M02,n1));
+		l.push_front(T3<int>(m12,M12,n0));
+	}
+	// lexicographical ordering in O(NlogN) where N is the container size
+	l.sort();
+	
+	int a1 = (i+1)%3, a2 = (i+2)%3;
+	// cout << "Popping out the elements in l:"<<endl;
+	while (!l.empty() && (l.front()[0] != T[a1] || l.front()[1] != T[a2] || l.front()[2] == T[i])){
+		// cout << l.front() << endl;
+		l.pop_front();
+	}
+	
+	// list<T3<int>>::iterator it = l.begin();
+	// cout << *it << endl;
+	return !l.empty() ? l.front() : T;
+}
+
+T3<int> Promenade(const T3<int> & T, const T3<double> & p){
+	cout << T << endl;
+	double a1, a2, a3;
+	T3<double> c1 = sommets[T[0]], c2 = sommets[T[1]], c3 = sommets[T[2]];
+	a1 = c1.produit_mixte(c2,p);
+	a2 = c2.produit_mixte(c3,p);
+	a3 = c3.produit_mixte(c1,p);
+	
+	if (a1 > 0 && a2 > 0 && a3 > 0){
+		return T;
+	} else {
+		int i = rand()%3; // random number between 0 and 2
+		T3<int> RandomNeighbor = Adjacency(T,i);
+		return Promenade(RandomNeighbor,p);
+	}
 }
 
 };
--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(){
 	char* const pnom = nom;
     Maillage m(pnom);
 	// T3<double>* Sommets = m.GetSommets();
-	// T3<int>* Triangles = m.GetTriangles();
+	T3<int>* Triangles = m.GetTriangles();
 	
 //	cout << " Number Sommets " << m.GetNumbSommets() << endl;
 //	for (int i = 0; i < m.GetNumbSommets(); i++){
@@ -20,8 +20,18 @@ int main(){
 //	for (int i = 0; i < m.GetNumbTri(); i++){
 //		cout << Triangles[i] << endl;
 //	}
+
+	T3<int> T = T3<int>(1,18,5); // also in Maillage1: 1 18 16
+	// T3<int> T = Triangles[5];
+	T3<int> N = m.Adjacency(T,2);
+	cout << T << endl;
+	cout << N << endl;
 	
-    exportGnuplot(m);
+	// const T3<double> p = T3<double>(0,0,0);
+	// T3<int> P = m.Promenade(T, p); // Fehler in Wahl des Nachbardreiecks, Abbruchbedingung einfügen
+	// cout << P << endl;
+	
+    // exportGnuplot(m);
     
     return 0;
 }
