c285c4155a9b6199c843af6c284b0b773b19f146
okauf <olivia.kaufmann@web.de>
2017-11-29 15:25:29 +0100

    Testdateien Maillage

--- main.cpp
+++ main.cpp
@@ -5,11 +5,7 @@ using namespace std;
 int main(){
     
     Maillage m("maillage2.msh");
-	T3<double> vec, vec2(0,1.,3);
-	T3<int> vec3(1,2,3), vec4(0.,1,2);
-	cout << vec << endl;
-	cout << vec2 << endl;
-	cout << vec3 << endl;
-	cout << vec4 << endl;
+	// T3<double> vec;
+	// T3<int> vec2(1,2,3);
     return 0;
 }
