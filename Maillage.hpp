ab1b18de7f7ef9a759803171c947324da34fbfd6
okauf <olivia.kaufmann@web.de>
2017-12-05 18:00:35 +0100

    Promenade

--- Maillage.hpp
+++ Maillage.hpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <list>
 #include <map>
+#include <vector>
 using namespace std;
 
 class Maillage{
@@ -107,56 +108,47 @@ Triangle* LoadTriangles(char* const input){
     return triangles;
 }
 
-//Triangle Adjacency(const Triangle & T, int i){
-//	
-//	assert(i <= 2 && i >= 0);
-//	
-//	// initialisation
-//	list<Triangle> l;
-//	int n0, n1, n2, m01, m02, m12, M01, M02, M12;
-//	for (int i = 0; i < numbTri; i++){
-//		n0 = triangles[i][0];
-//		n1 = triangles[i][1];
-//		n2 = triangles[i][2];
-//		m01 = min(n0,n1); m02 = min(n0,n2); m12 = min(n1,n2);
-//		M01 = max(n0,n1); M02 = max(n0,n2); M12 = max(n1,n2);
-//		l.push_front(Triangle(m01,M01,n2));
-//		l.push_front(Triangle(m02,M02,n1));
-//		l.push_front(Triangle(m12,M12,n0));
-//	}
-//	// lexicographical ordering in O(NlogN) where N is the container size
-//	l.sort();
-//	
-//	int a1 = (i+1)%3, a2 = (i+2)%3;
-//	// cout << "Popping out the elements in l:"<<endl;
-//	while (!l.empty() && (l.front()[0] != T[a1] || l.front()[1] != T[a2] || l.front()[2] == T[i])){
-//		// cout << l.front() << endl;
-//		l.pop_front();
-//	}
-//	
-//	// list<Triangle>::iterator it = l.begin();
-//	// cout << *it << endl;
-//	return !l.empty() ? l.front() : T;
-//}
-//
-//Triangle Promenade(const Triangle & T, const T3<double> & p){
-//	cout << T << endl;
-//	double a1, a2, a3;
-//	T3<double> c1 = sommets[T[0]], c2 = sommets[T[1]], c3 = sommets[T[2]];
-//	a1 = c1.produit_mixte(c2,p);
-//	a2 = c2.produit_mixte(c3,p);
-//	a3 = c3.produit_mixte(c1,p);
-//	
-//	if (a1 > 0 && a2 > 0 && a3 > 0){
-//		return T;
-//	} else {
-//		int i = rand()%3; // random number between 0 and 2
-//		Triangle RandomNeighbor = Adjacency(T,i);
-//		return Promenade(RandomNeighbor,p);
-//	}
-//}
-//
-//
+
+Triangle Promenade(const Triangle & T, const T3<double> & p, vector<Triangle> path){
+	cout << T << endl;
+	path.push_back(T);
+	
+	T3<double> c1 = sommets[T[0]], c2 = sommets[T[1]], c3 = sommets[T[2]];
+	double a1 = c1.produit_mixte(c2,p);
+	double a2 = c2.produit_mixte(c3,p);
+	double a3 = c3.produit_mixte(c1,p);
+	
+	if (a1 >= 0 && a2 >= 0 && a3 >= 0){
+		return T;
+	} else {
+		// evtl rand()
+		if (a1 < a2 && a1 < a3){
+			Triangle RandomNeighbor = T.getNeighbor3();
+		} else if ( a2 < a1 && a2 < a3){
+			Triangle RandomNeighbor = T.getNeighbor1();
+		} else {
+			Triangle RandomNeighbor = T.getNeighbor2();
+		}
+		return Promenade(RandomNeighbor, p, path);
+	}
+}
+
+// Triangle Promenade(const Triangle & T, const T3<double> & p){
+	// cout << T << endl;
+	// double a1, a2, a3;
+	// T3<double> c1 = sommets[T[0]], c2 = sommets[T[1]], c3 = sommets[T[2]];
+	// a1 = c1.produit_mixte(c2,p);
+	// a2 = c2.produit_mixte(c3,p);
+	// a3 = c3.produit_mixte(c1,p);
+	
+	// if (a1 > 0 && a2 > 0 && a3 > 0){
+		// return T;
+	// } else {
+		// int i = rand()%3; // random number between 0 and 2
+		// Triangle RandomNeighbor = Adjacency(T,i);
+		// return Promenade(RandomNeighbor,p);
+	// }
+// }
     
 };
 
@@ -262,7 +254,7 @@ void setAdjacencyViaList(Maillage m){
 	while (!adjacency.empty()){
 		curr_tri = adjacency.front();
 		adjacency.pop_front();
-		// cout << prev_tri << " next: " << curr_tri << endl;
+		cout << prev_tri << " next: " << curr_tri << endl;
 		if (curr_tri[0] == prev_tri[0] && curr_tri[1] == prev_tri[1]){
 			
 			// triangles[curr_tri[2]].setNeighbor(prev_tri[2]);
