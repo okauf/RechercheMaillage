3aad525a2b1fa94123b64c32919f96d09cd8a7e0
Arne Heimendahl <arneheimendahl@gmx.de>
2017-12-19 15:58:42 +0100

    xxx

--- T3.hpp
+++ T3.hpp
@@ -65,28 +65,28 @@ int min_neg(const double & x, const double & y, const double & z){
 	} else { return 3;}
 };
 
-int random_neg(const double & x, const double & y, const double & z){
-    
-    int boolx = (x < 0)?1:0, booly = (y < 0)?1:0, boolz = (z < 0)?1:0;
-    if (boolx == 0 && booly == 0 && boolz == 0){
-        return -1;
-	 }
-    int neg_numb = boolx + booly + boolz;
-    if (neg_numb  == 1){
-        if(boolx == 1){ return 1; }
-        if(booly == 1){ return 2; }
-        if(boolz == 1){ return 3; }
-    } else {
-		 struct timespec nanos;
-		 clock_gettime(CLOCK_MONOTONIC, &nanos);
-		 srand(nanos.tv_nsec);
-        int rand_numb = rand()%2; cout << rand_numb << " ";
-        if(boolx == 0){ return rand_numb + 2; }
-        if(booly == 0){ return 2*rand_numb + 1; }
-        if(boolz == 0){ return rand_numb + 1; }
-    }
-	 return -2; // XXXXX avoid warning
-};
+//int random_neg(const double & x, const double & y, const double & z){
+//    
+//    int boolx = (x < 0)?1:0, booly = (y < 0)?1:0, boolz = (z < 0)?1:0;
+//    if (boolx == 0 && booly == 0 && boolz == 0){
+//        return -1;
+//	 }
+//    int neg_numb = boolx + booly + boolz;
+//    if (neg_numb  == 1){
+//        if(boolx == 1){ return 1; }
+//        if(booly == 1){ return 2; }
+//        if(boolz == 1){ return 3; }
+//    } else {
+//		 struct timespec nanos;
+//		 clock_gettime(CLOCK_MONOTONIC, &nanos);
+//		 srand(nanos.tv_nsec);
+//        int rand_numb = rand()%2; cout << rand_numb << " ";
+//        if(boolx == 0){ return rand_numb + 2; }
+//        if(booly == 0){ return 2*rand_numb + 1; }
+//        if(boolz == 0){ return rand_numb + 1; }
+//    }
+//	 return -2; // XXXXX avoid warning
+//};
 
 //Class triangle which is a derived class from the class T3<int>; it has as additional members the neighbors
 class Triangle: public T3<int>{
