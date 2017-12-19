dc1cefccda411fea561108d8a1086d09d6000e50
Arne Heimendahl <arneheimendahl@gmx.de>
2017-12-19 15:34:35 +0100

    testumgebung fertig

--- main.cpp
+++ main.cpp
@@ -90,9 +90,51 @@ int main(){
             
             cout << "The point " << p << " is in the triangle with vertices " << endl << sommets[P[0]-1] << endl << sommets[P[1]-1] << endl << sommets[P[2]-1] << endl;
             exportGnuplot(m,path,&p,1);
+        }
             
+        case 3: {
+            cout << "Choose two maillage files, the maillages become finer with increasing index. " << endl;
+            cout << "Choose the maillage whose sommets should be covered by triangles of the finer maillage " << endl << "1: maillage1.msh" << endl << "2: maillage2.msh" << endl << "3: maillage3.msh" << endl << "4: maillage4.msh" << endl;
+            int mOption;
+            cin >> mOption;
+            assert(mOption >= 0 && mOption < 5);
+            string name;
+            switch (mOption) {
+                case 1: {name = "maillage1.msh";    break;}
+                case 2: {name = "maillage2.msh";    break;}
+                case 3: {name = "maillage3.msh";    break;}
+                case 4: {name = "maillage4.msh";    break;}
+            }
+            
+            Maillage m(name);
+            
+            
+            cout << "Chose the finer maillage file " << endl << "1: maillage1.msh" << endl << "2: maillage2.msh" << endl << "3: maillage3.msh" << endl << "4: maillage4.msh" << endl;
+            cin >> mOption;
+            assert(mOption >= 0 && mOption < 5);
+            switch (mOption) {
+                case 1: {name = "maillage1.msh";    break;}
+                case 2: {name = "maillage2.msh";    break;}
+                case 3: {name = "maillage3.msh";    break;}
+                case 4: {name = "maillage4.msh";    break;}
+            }
+            Maillage M(name);
+            setAdjacencyViaMultiMap(m); setAdjacencyViaList(M);
+            
+            vector<Triangle> coveringTriangles;
+            for(int i = 0; i <m.GetNumbSommets();i++)
+                coveringTriangles.push_back(Triangle(0,0,0));
+            
+            coveringTriangles = findSommets(m,M,coveringTriangles);
+            
+            exportGnuplot(M,coveringTriangles,m.GetSommets(),m.GetNumbSommets());
+            
+            
+            
+
             
         }
+            
         
             
             
