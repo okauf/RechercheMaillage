8db691b4c2dff9dd21e189aca7d1a63cbf7d00c6
okauf <olivia.kaufmann@web.de>
2017-12-19 19:11:01 +0100

    time meas

--- T3.hpp
+++ T3.hpp
@@ -88,12 +88,12 @@ int random_neg(const double & x, const double & y, const double & z){
        if(booly == 1){ return 2; }
        if(boolz == 1){ return 3; }
    } else {
-	   // struct timespec nanos;
-	   // clock_gettime(CLOCK_MONOTONIC, &nanos);
-	   // srand(nanos.tv_nsec);
-	   srand(time(NULL));
+	   struct timespec nanos;
+	   clock_gettime(CLOCK_MONOTONIC, &nanos);
+	   srand(nanos.tv_nsec);
+	   // srand(time(NULL));
        int rand_numb = rand()%2;
-	   cout << rand_numb << " ";
+	   // cout << rand_numb << " ";
        if(boolx == 0){ return rand_numb + 2; }
        if(booly == 0){ return 2*rand_numb + 1; }
        if(boolz == 0){ return rand_numb + 1; }
--- TimeMeasurements.cpp
+++ TimeMeasurements.cpp
@@ -13,25 +13,40 @@ int main(){
 string name = "maillage4.msh";
 Maillage m(name);
 m.setAdjacencyViaList();
+int numbExp = 10;
 
+// random starting triangles
 int numbTri = m.GetNumbTri();
-vector<int> randomTriIndex;
-for (int i = 0; i < 10; i++){
-	srand(time(NULL));
-	randomTriIndex.push_back(rand()%numbTri);
-}
-
-vector<T3<double>> randomSommets;
+int * randomTriIndex = new int[numbExp];
+// random points in the unit disk to be covered
+T3<double> * randomSommets = new T3<double>[numbExp];
 double radius, angle;
 srand(time(NULL));
-for (int i = 0; i < 10; i++){
+for (int i = 0; i < numbExp; i++){
+	int help = rand()%numbTri;
+	randomTriIndex[i] = help;
+	// cout << help << " ";
+	
 	radius = (rand() % 10000) / 10000.0;
 	angle = 2 * M_PI * (rand() % 10000) / 10000.0;	
-	T3<double> randomSommet(radius*cos(angle),radius*sin(angle),0);
-	cout << randomSommet << endl;
-	randomSommets.push_back(randomSommet);
+	randomSommets[i] = T3<double>(radius*cos(angle),radius*sin(angle),0);
+	// cout << randomSommets[i] << endl;
 }
-	
+
+Triangle* triangles = m.GetTriangles();
+vector<Triangle> path;
+int * path_length = new int[numbExp];
+double * running_time = new double[numbExp];
+for (int i = 0; i < numbExp; i++){
+	path.clear();
+	auto t1 = high_resolution_clock::now();
+	Triangle P = m.Promenade(triangles[randomTriIndex[i]],randomSommets[i],path);
+	auto t2 = high_resolution_clock::now();
+	duration<double> durationIns = t2-t1;
+	running_time[i] = durationIns.count();
+	path_length[i] = path.size();
+	cout << running_time[i] << " " << path_length[i] << endl;
+}
 	
 }
 
