557bca2a49242340dd083b9d37dcd8f66dac367f
okauf <olivia.kaufmann@web.de>
2018-01-02 18:51:02 +0100

    random generation of starting triangles and points to cover

--- TimeMeasurements.cpp
+++ TimeMeasurements.cpp
@@ -13,13 +13,13 @@ void runPromenade(Maillage & m, string input, int numbExp, int * path_length, do
 
 int main(){
 	
-string name = "maillage4.msh";
+string name = "maillage5.msh";
 Maillage m(name);
 m.setAdjacencyViaList();
 int numbExp = 10;
 
 string input = "randomData.txt";
-// auskommentieren um min_neg und random_neg zu vergleichen
+// do not execute defineTriandPoints in order to compare the results of min_neg and random_neg
 // defineTriandPoints(m, input, numbExp);
 
 int * path_length = new int[numbExp];
@@ -62,30 +62,33 @@ void runPromenade(Maillage & m, string input, int numbExp, int * path_length, do
 	
 	Triangle StartTri;
 	T3<double> p;
-	stringstream linestream;
 	string line;
 	int idx;
 	double a, b, c;
-	// auto t1, t2; // time measurement
 	duration<double> durationIns;
 	Triangle P;
 	
 	for (int i = 0; i < numbExp; i++){
 		path.clear();
 		
+		// reading list position of the starting triangle
 		getline(randomData,line);
 		idx = stoi(line);
 		StartTri = triangles[idx];
-		cout << "idx " << idx << endl;
+		// cout << "idx " << idx << endl;
 		
+		// reading point to cover
 		getline(randomData,line);
+		stringstream linestream;
 		linestream << line;
 		linestream >> a >> b >> c;
 		p = T3<double>(a,b,c);
-		cout << "p " << p << endl;
+		// cout << "p " << p << endl;
 		
+		// skip blank line
 		getline(randomData,line);
 		
+		// time measurement and path length for method promenade
 		auto t1 = high_resolution_clock::now();
 		P = m.Promenade(StartTri,p,path);
 		auto t2 = high_resolution_clock::now();
